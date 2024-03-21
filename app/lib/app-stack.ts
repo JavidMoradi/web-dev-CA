07432237e13d62c7ec92fb088339d60eb9aaf639
Javid M <javidmoradi0@gmail.com>
2024-03-21 20:24:43 +0300

    Add dynamoDB and seed

--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,6 +1,11 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from "../shared/utils";
+import { movieReviews } from "../seed/movieReviews";
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +13,46 @@ export class AppStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // dummy lambda function
+    const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambdas/simple.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
+
+    const simpleFnURL = simpleFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
+
+    // dynamoDB Table
+    const movieReviewsTable = new dynamodb.Table(this, "MovieReviewsTable", {
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      tableName: "MovieReviews",
+    });
+
+    new custom.AwsCustomResource(this, "moviesddbInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [movieReviewsTable.tableName]: generateBatch(movieReviews),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("moviesddbInitData"),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [movieReviewsTable.tableArn],
+      }),
+    });
   }
 }
---
+++ app/seed/movieReviews.ts
@@ -0,0 +1,18 @@
+import { MovieReview } from "../shared/types";
+
+export const movieReviews: MovieReview[] = [
+    {
+        id: 998, 
+        reviewer_name: "John Doe",
+        reviewer_date: "2023-10-20",
+        content: "Best movie to ever exist!",
+        rating: 5
+    },
+    {
+        id: 999,
+        reviewer_name: "Michael Jackson",
+        reviewer_date: "2023-11-22",
+        content: "It was a fun family movie, but nothing fancy.",
+        rating: 3
+    }
+]
